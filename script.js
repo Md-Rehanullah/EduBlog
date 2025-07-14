c07de0d2a85612cf1e7e1f1187ea0dc5f0141d5a
Md Rehanullah <157832474+Md-Rehanullah@users.noreply.github.com>
2025-07-14 17:44:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -716,3 +716,24 @@ window.EduBlog = {
     showToast,
     openPostModal
 };
+
+
+//extra parsts
+const scrollToTopButton = document.getElementById('scrollToTop');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+});
+
+scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+
+
+
+
